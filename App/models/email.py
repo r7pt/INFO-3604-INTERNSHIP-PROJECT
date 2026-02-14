0e771a617591983da910457020e486b3b133bc9f
r7pt <125412480+r7pt@users.noreply.github.com>
2026-02-14 20:52:46 +0000

    added email getter, still not complete

--- App/models/email.py
+++ App/models/email.py
@@ -2,19 +2,22 @@ from App.database import db
 from datetime import date
 from email_validator import validate_email, EmailNotValidError
 from sqlalchemy_imageattach.entity import Image, image_attachment
+from App.models.user import user
 
-class(db.model):
-    email_id = db.column(db.Integer,primary_key=True)
-    student_email = db.column(db.String,ForeignKey("user.student_id"),nullable = True )
-    company_email = db.column(db.String,ForeignKey("user.company_id"),nullable = True)
-    subject = db.column(db.String(50),nullable = False)
-    description = db.column(db.String(500),nullable = False)
-    graphic = db.column(image_attachment("graphic"),nullable=True)
-    attachment = db.column(db.String,nullable = True)
+class Email(db.Model):
+    email_id = db.Column(db.Integer,primary_key=True)
+    recipient_id= db.Column(db.Integer,ForeignKey("user.user_id"),nullable =False)
+    sender_id = db.Column(db.Integer,ForeignKey("user.user_id"),nullable = False)
+    subject = db.Column(db.String(50),nullable = False)
+    description = db.Column(db.String(500),nullable = False)
+    graphic = db.Column(image_attachment("graphic"),nullable=False)
+    attachment = db.Column(db.String,nullable = True)
+    created_at = db.Column(db.DateTime, default= db.DateTime.utcnow)
+    status = db.Column(db.Boolean, nullable =False, default= False)
+    
+    sender = db.relationship("User",backref="sent_emails",foreign_keys =[sender_id],lazy=True)
+    recipient= db.relationship("User",backref="received_emails",foreign_keys=[recipient_id],lazy=True)
     __tablename__ = 'email'
-
-    student = db.relationship("Student",backrefs=("emails"),foreign_keys =[student_id],lazy=True)
-    company = db.relationship("Company",backrefs="emails",foreign_keys=[company_id],lazy=True)
 
     def __init__(self,student_email,company_email,subject,description,graphic,attachment):
         self.student_email= student_email,
@@ -23,6 +26,80 @@ class(db.model):
         self.description=description,
         self.graphic=graphic,
         self.attachment=attachment
+
+    #get all email 
+    def get_all_emails(self):
+        emails= Email.query.all()
+        if not emails:
+            return []
+        return emails
+
+    #get all email sent by company
+    def get_company_sent_emails(self,company_id):
+        try:
+            #email = Email.query.join(User, Email.sender_id == user.user_id).filter(user.user_id==company_id).all()
+            user = User.query.get(company_id)
+            if not user:
+                print("no company found")
+                return[]
+            emails = user.sent_emails
+            if not emails :
+                return []
+            return emails
+        except Exception as e:
+            print("Error retrieving  emails:",e)
+            return []
+
+    #get all emails sent by staff
+    def get_staff_sent email(self,staff_id):
+        try:
+            staff_user = User.query.get(staff_id)
+            if not staff_user:
+                print("staff not found")
+                return[]
+            emails = staff.sent_emails
+            if not emails:
+                print("staff has sent zero emails")
+                return []
+            return emails
+        except Exception as e 
+            print("exception", e)
+            return []
+
+    #get all emails sent to company
+    def get_company_receival_emails(self,company_id)
+        try:
+            company = User.query.get(company_id)
+            if not company:
+                print("company not found")
+            emails = company.received_emails
+            if not emails:
+                print("company has recieved zero emails")
+                return []
+            return emails
+        except Exception as e 
+            print("exception", e)
+            return []
+    
+    #get all email sent to student
+    def get_all_student_emails(self,student_id)
+        try:
+            student = User.query.get(student_id)
+            if not student:
+                print("student not found")
+            emails = student.received_emails
+            if not emails:
+                print("student has recieved zero emails")
+                return []
+            return emails
+        except Exception as e 
+            print("exception", e)
+            return []
+  
+# get inbox
+
+  #get email by header
+    
 
     def get_json(self)
         email = {

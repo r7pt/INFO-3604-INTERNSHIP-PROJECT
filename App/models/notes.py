a7cec03d3b7fddb96efcf72a621195c1a905a866
r7pt <125412480+r7pt@users.noreply.github.com>
2026-02-15 20:15:02 +0000

    add meeting relationship

--- App/models/notes.py
+++ App/models/notes.py
@@ -2,6 +2,7 @@ from App.database import db
 from App.models.user import User
 from App.models.student import Student
 from App.models.staff import Staff
+from App.models.meeting import Meeting
 from sqlalchemy.ext.hybrid import hybrid_property
 from datetime import date
 
@@ -9,13 +10,15 @@ class Notes(db.Model):
     note_id = db.Column(db.Integer,primary_key=True)
     student_id = db.Column(db.Integer,db.ForeignKey=("student.id"),nullable=False)
     staff_id =db.Column(db.Integer,db.ForeignKey=("staff.staff_id"),nullable=False)
+    meeting_id =db.Column(db.Integer,db.ForeignKey=("meeting.id"),nullable=False) 
     description = db.Column(db.String,nullable=False)
     parent_id = db.Column(db.Integer,db.ForeignKey=("notes.note_id"),nullable=True)
     created_at= db.Column(db.DateTime,default=db.DateTime.utcnow,nullable=False)
 
     staff= db.relationship('Staff',foreign_keys=['staff_id'],backref="created_by")
     student = db.relationship('Student',foreign_keys=['student_id'],backref="notes")
-    parent =db.relationship("Notes",remote_side=[note_id],backref="children")
+    parent =db.relationship("Notes",remote_side=['note_id'],backref="children")
+    meeting = db.relationship("Meeting", foreign_keys=['meeting_id'],backref="notes")
 
 
 
@@ -29,6 +32,17 @@ class Notes(db.Model):
     def get_note_by_id(note_id):
         note= Notes.query.get(note_id)
         return note
+
+    def get_meeting(note_id):
+        note=Notes.query.get(note_id)
+        if not note:
+            print("no note found with id",note_id)
+                return[]
+            meeting = note.meeting
+            return meeting
+        except Exception as e:
+            print("an error occurred ",e)
+
 
     def get_all_student_notes(student_id):
         try:

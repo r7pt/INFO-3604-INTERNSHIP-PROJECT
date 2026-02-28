38304270e0f376c75daf08aa48a00b442b676d23
TragicVibesHD <shivan562@yahoo.com>
2026-02-28 18:33:48 -0400

    Forgot-To-Include-The-Email-Feild-For-This-Class

--- App/models/companyRegistration.py
+++ App/models/companyRegistration.py
@@ -1,31 +1,35 @@
 from App.database import db
 from datetime import datetime
 
+
 class CompanyRegistration(db.Model):
-    tablename = 'company_registration'
+    __tablename__ = "company_registration"
 
     id = db.Column(db.Integer, primary_key=True)
     company_name = db.Column(db.String(256), nullable=False)
+    email = db.Column(db.String(256), nullable=False, unique=True, index=True)
     website = db.Column(db.String(256))
     category = db.Column(db.String(256))
 
     created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
-    projects = db.relationship('Project', backref='registration', lazy=True)
+    projects = db.relationship("Project", backref="registration", lazy=True)
 
-    def init(self, company_name, website, category):
+    def __init__(self, company_name, email, website=None, category=None):
         self.company_name = company_name
+        self.email = email
         self.website = website
         self.category = category
 
     def get_json(self):
         return {
-            'id': self.id,
-            'company_name': self.company_name,
-            'website': self.website,
-            'category': self.category,
-            'created_at': self.created_at.isoformat()
+            "id": self.id,
+            "company_name": self.company_name,
+            "email": self.email,
+            "website": self.website,
+            "category": self.category,
+            "created_at": self.created_at.isoformat()
         }
 
-    def repr(self):
+    def __repr__(self):
         return f"<CompanyRegistration {self.id}: {self.company_name}>"
